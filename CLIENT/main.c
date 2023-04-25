79fe8323851f8c306c03919511118bf2a80cdbc5
Truyensbram <121112258+Truyensbram@users.noreply.github.com>
2023-04-25 11:22:51 +0200

    test GPT

--- CLIENT/main.c
+++ CLIENT/main.c
@@ -150,3 +150,93 @@ void cleanup( int internet_socket, struct sockaddr * internet_address )
 	//Step 3.1
 	close( internet_socket );
 }
+\*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+#define PORT 12345 // het poortnummer waarop de UDP-server luistert
+#define BUF_SIZE 1024 // de maximale grootte van de buffer
+
+int main() {
+    struct sockaddr_in server_addr, client_addr;
+    int sockfd, recvlen, highest_num = 0;
+    socklen_t addrlen = sizeof(client_addr);
+    char buf[BUF_SIZE];
+    struct timeval tv;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        printf("Waiting for message...\n");
+
+        memset(buf, 0, BUF_SIZE);
+
+        recvlen = recvfrom(sockfd, buf, BUF_SIZE, 0, (struct sockaddr *)&client_addr, &addrlen);
+
+        if (recvlen > 0) {
+            printf("Received message: %s\n", buf);
+
+            if (strcmp(buf, "GO") == 0) {
+                printf("Sending reply...\n");
+
+                highest_num = 0;
+                tv.tv_sec = 1;
+                tv.tv_usec = 0;
+
+                while (1) {
+                    sendto(sockfd, &highest_num, sizeof(int), 0, (struct sockaddr *)&client_addr, addrlen);
+                    printf("Sent highest_num = %d\n", highest_num);
+
+                    if (select(sockfd + 1, &tv, NULL, NULL, NULL) <= 0) {
+                        // Er is geen inkomende data meer ontvangen binnen de timeout van 1 seconde.
+                        // Stuur het hoogste ontvangen getal 3 keer terug en stop met wachten.
+                        for (int i = 0; i < 3; i++) {
+                            sendto(sockfd, &highest_num, sizeof(int), 0, (struct sockaddr *)&client_addr, addrlen);
+                            printf("Sent highest_num = %d\n", highest_num);
+                        }
+                        break;
+                    }
+
+                    // Er is inkomende data ontvangen binnen de timeout van 1 seconde.
+                    // Ontvang het inkomende getal en update het hoogste ontvangen getal indien nodig.
+                    int num;
+                    recvfrom(sockfd, &num, sizeof(int), 0, (struct sockaddr *)&client_addr, &addrlen);
+                    printf("Received num = %d\n", num);
+
+                    if (num > highest_num) {
+                        highest_num = num;
+                    }
+
+                    if (strcmp(buf, "OK") == 0) {
+                        printf("Received OK, breaking out of loop.\n");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    close(sockfd);
+    return 0;
+}
+*\
