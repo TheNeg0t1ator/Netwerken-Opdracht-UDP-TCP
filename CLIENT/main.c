1bfd70e01896e445d448b85cacd4f8182467a742
Truyensbram <121112258+Truyensbram@users.noreply.github.com>
2023-05-01 23:19:31 +0200

    last update

--- CLIENT/main.c
+++ CLIENT/main.c
@@ -1,156 +1,3 @@
-#ifdef _WIN32
-	#define _WIN32_WINNT _WIN32_WINNT_WIN7
-	#include <winsock2.h> //for all socket programming
-	#include <ws2tcpip.h> //for getaddrinfo, inet_pton, inet_ntop
-	#include <stdio.h> //for fprintf, perror
-	#include <unistd.h> //for close
-	#include <stdlib.h> //for exit
-	#include <string.h> //for memset
-	void OSInit( void )
-	{
-		WSADATA wsaData;
-		int WSAError = WSAStartup( MAKEWORD( 2, 0 ), &wsaData ); 
-		if( WSAError != 0 )
-		{
-			fprintf( stderr, "WSAStartup errno = %d\n", WSAError );
-			exit( -1 );
-		}
-	}
-	void OSCleanup( void )
-	{
-		WSACleanup();
-	}
-	#define perror(string) fprintf( stderr, string ": WSA errno = %d\n", WSAGetLastError() )
-#else
-	#include <sys/socket.h> //for sockaddr, socket, socket
-	#include <sys/types.h> //for size_t
-	#include <netdb.h> //for getaddrinfo
-	#include <netinet/in.h> //for sockaddr_in
-	#include <arpa/inet.h> //for htons, htonl, inet_pton, inet_ntop
-	#include <errno.h> //for errno
-	#include <stdio.h> //for fprintf, perror
-	#include <unistd.h> //for close
-	#include <stdlib.h> //for exit
-	#include <string.h> //for memset
-	void OSInit( void ) {}
-	void OSCleanup( void ) {}
-#endif
-
-int initialization( struct sockaddr ** internet_address, socklen_t * internet_address_length );
-void execution( int internet_socket, struct sockaddr * internet_address, socklen_t internet_address_length );
-void cleanup( int internet_socket, struct sockaddr * internet_address );
-
-int main( int argc, char * argv[] )
-{
-	//////////////////
-	//Initialization//
-	//////////////////
-
-	OSInit();
-
-	struct sockaddr * internet_address = NULL;
-	socklen_t internet_address_length = 0;
-	int internet_socket = initialization( &internet_address, &internet_address_length );
-
-	/////////////
-	//Execution//
-	/////////////
-
-	execution( internet_socket, internet_address, internet_address_length );
-
-
-	////////////
-	//Clean up//
-	////////////
-
-	cleanup( internet_socket, internet_address );
-
-	OSCleanup();
-
-	return 0;
-}
-
-int initialization( struct sockaddr ** internet_address, socklen_t * internet_address_length )
-{
-	//Step 1.1
-	struct addrinfo internet_address_setup;
-	struct addrinfo * internet_address_result;
-	memset( &internet_address_setup, 0, sizeof internet_address_setup );
-	internet_address_setup.ai_family = AF_UNSPEC;
-	internet_address_setup.ai_socktype = SOCK_DGRAM;
-	int getaddrinfo_return = getaddrinfo( "::1", "24042", &internet_address_setup, &internet_address_result );
-	if( getaddrinfo_return != 0 )
-	{
-		fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
-		exit( 1 );
-	}
-
-	int internet_socket = -1;
-	struct addrinfo * internet_address_result_iterator = internet_address_result;
-	while( internet_address_result_iterator != NULL )
-	{
-		//Step 1.2
-		internet_socket = socket( internet_address_result_iterator->ai_family, internet_address_result_iterator->ai_socktype, internet_address_result_iterator->ai_protocol );
-		if( internet_socket == -1 )
-		{
-			perror( "socket" );
-		}
-		else
-		{
-			//Step 1.3
-			*internet_address_length = internet_address_result_iterator->ai_addrlen;
-			*internet_address = (struct sockaddr *) malloc( internet_address_result_iterator->ai_addrlen );
-			memcpy( *internet_address, internet_address_result_iterator->ai_addr, internet_address_result_iterator->ai_addrlen );
-			break;
-		}
-		internet_address_result_iterator = internet_address_result_iterator->ai_next;
-	}
-
-	freeaddrinfo( internet_address_result );
-
-	if( internet_socket == -1 )
-	{
-		fprintf( stderr, "socket: no valid socket address found\n" );
-		exit( 2 );
-	}
-
-	return internet_socket;
-}
-
-void execution( int internet_socket, struct sockaddr * internet_address, socklen_t internet_address_length )
-{
-	//Step 2.1
-	int number_of_bytes_send = 0;
-	number_of_bytes_send = sendto( internet_socket, "Hello UDP world!", 16, 0, internet_address, internet_address_length );
-	if( number_of_bytes_send == -1 )
-	{
-		perror( "sendto" );
-	}
-
-	//Step 2.2
-	int number_of_bytes_received = 0;
-	char buffer[1000];
-	number_of_bytes_received = recvfrom( internet_socket, buffer, ( sizeof buffer ) - 1, 0, internet_address, &internet_address_length );
-	if( number_of_bytes_received == -1 )
-	{
-		perror( "recvfrom" );
-	}
-	else
-	{
-		buffer[number_of_bytes_received] = '\0';
-		printf( "Received : %s\n", buffer );
-	}
-}
-
-void cleanup( int internet_socket, struct sockaddr * internet_address )
-{
-	//Step 3.2
-	free( internet_address );
-
-	//Step 3.1
-	close( internet_socket );
-}
-\*
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -194,10 +41,10 @@ int main() {
         recvlen = recvfrom(sockfd, buf, BUF_SIZE, 0, (struct sockaddr *)&client_addr, &addrlen);
 
         if (recvlen > 0) {
-            printf("Received message: %s\n", buf);
+            
 
             if (strcmp(buf, "GO") == 0) {
-                printf("Sending reply...\n");
+                
 
                 highest_num = 0;
                 tv.tv_sec = 1;
@@ -205,14 +52,13 @@ int main() {
 
                 while (1) {
                     sendto(sockfd, &highest_num, sizeof(int), 0, (struct sockaddr *)&client_addr, addrlen);
-                    printf("Sent highest_num = %d\n", highest_num);
-
+                    
                     if (select(sockfd + 1, &tv, NULL, NULL, NULL) <= 0) {
                         // Er is geen inkomende data meer ontvangen binnen de timeout van 1 seconde.
                         // Stuur het hoogste ontvangen getal 3 keer terug en stop met wachten.
                         for (int i = 0; i < 3; i++) {
                             sendto(sockfd, &highest_num, sizeof(int), 0, (struct sockaddr *)&client_addr, addrlen);
-                            printf("Sent highest_num = %d\n", highest_num);
+                            
                         }
                         break;
                     }
@@ -221,14 +67,14 @@ int main() {
                     // Ontvang het inkomende getal en update het hoogste ontvangen getal indien nodig.
                     int num;
                     recvfrom(sockfd, &num, sizeof(int), 0, (struct sockaddr *)&client_addr, &addrlen);
-                    printf("Received num = %d\n", num);
+                    
 
                     if (num > highest_num) {
                         highest_num = num;
                     }
 
                     if (strcmp(buf, "OK") == 0) {
-                        printf("Received OK, breaking out of loop.\n");
+                        
                         break;
                     }
                 }
@@ -239,4 +85,3 @@ int main() {
     close(sockfd);
     return 0;
 }
-*\
